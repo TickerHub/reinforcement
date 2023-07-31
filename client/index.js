882894646d7bbd4060f4564678461df40d1a07ec
Jordan Lopez <jordanlopez992@gmail.com>
2023-07-31 14:55:35 -0400

    Webpack functionality

--- client/index.js
+++ client/index.js
@@ -1,4 +1,6 @@
 import React from "react";
+import * as ReactDOM from "react-dom/client";
 import App from "./components/App";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-render(<App />, document.getElementById("root"));
+root.render(<App />, document.getElementById("root"));
