33bca79ff5d24d2b9a260733743850746fd30160
Jordan Lopez <jordanlopez992@gmail.com>
2023-07-31 15:50:52 -0400

    Set up react router

--- server/index.js
+++ server/index.js
@@ -8,6 +8,14 @@ app.get("/", function (request, response) {
   response.send("Hello World!");
 });
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.resolve(__dirname, "../index.html"), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
+});
+
 app.listen(3000, function () {
   console.log("Started application on port %d", 3000);
 });
