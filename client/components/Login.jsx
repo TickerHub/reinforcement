50a1157d95482291d467d585bef988aa49194ff8
Alexis Contreras <contreras10alexis@gmail.com>
2023-08-01 12:30:46 -0400

    completed login page

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,24 +1,54 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { signInWithGoogle } from '../firebase.js';
+import Logo from '../../images/TickerHub-white (1).png';
+
 export default function Login() {
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     await signInWithGoogle().then(async (data) => {
-        console.log(data);
-        navigate('/home');
+      console.log(data);
+      navigate('/home');
     });
   };
   return (
-    <div className='login-wrapper'>
-      <h1>Welcome! Please Log In!</h1>
-      <button onClick={handleSubmit} variant='primary'>
-        Google Log In
-      </button>
-      {''}
-      <div>Login</div>
-    </div>
+    <>
+      <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8'>
+        <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
+          <img className='mx-auto h-30 w-auto' src={Logo} alt='Your Company' />
+          <h2 className='text-center text-2xl font-bold leading-9 tracking-tight text-white'>
+            Sign in with your Google account
+          </h2>
+        </div>
+
+        <div className='mt-10 sm:mx-auto sm:w-full sm:max-w-sm'>
+          <form className='space-y-6' action='#' method='POST'>
+            <div className='flex justify-center'>
+              <button class='px-4 py-2 border flex gap-2 border-slate-200 rounded-lg text-slate-700 bg-slate-300 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150'>
+                <img
+                  class='w-6 h-6'
+                  src='https://www.svgrepo.com/show/475656/google-color.svg'
+                  loading='lazy'
+                  alt='google logo'
+                />
+                <span>Login with Google</span>
+              </button>
+            </div>
+          </form>
+
+          <p className='mt-10 text-center text-sm text-gray-500'>
+            In collaboration with{' '}
+            <a
+              href='https://www.alphavantage.co/documentation/'
+              className='font-semibold leading-6 text-indigo-600 hover:text-indigo-500'
+            >
+              Alpha Vantage API
+            </a>
+          </p>
+        </div>
+      </div>
+    </>
   );
 }
