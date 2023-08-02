1a76b79b51d95a7185438d34d6b9385857f82e0a
jordanlope <jordanlopez992@gmail.com>
2023-08-01 20:19:42 -0400

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const path = require('path');
-const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express();
