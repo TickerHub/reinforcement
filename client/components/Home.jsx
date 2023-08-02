37f6961a7b217ffb1e7f7c3ba2d967b165c9c8fe
Alexis Contreras <contreras10alexis@gmail.com>
2023-08-02 15:24:18 -0400

    edited home page layout

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
-import Chat from './Chatroom/Chat';
 import { useNavigate } from 'react-router-dom';
+import Chat from './Chatroom/Chat';
 
 export default function Home(props) {
   const { setTickerData } = props;
@@ -27,37 +27,39 @@ export default function Home(props) {
   };
 
   return (
-    <div class='grid grid-cols-3 gap-4 divide-x-4'>
-      <div class='col-span-1 p-4 px-4'>
-        <div>TOP COMMENTS</div>
-      </div>
-      <div class='col-span-1'>
-        <div class='h-screen flex items-center justify-around px-4'>
-          <h1 class='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
-            Select Ticker
-          </h1>
-          <div class='form-control w-full max-w-xs'>
-            <input
-              onChange={onChange}
-              type='text'
-              placeholder='ex: TSLA'
-              class='input input-bordered w-full max-w-xs'
-            />
-            <button
-              onClick={onSubmit}
-              type='submit'
-              class='bg-indigo-500 flex-none rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 !important'
-            >
-              Search
-            </button>
+    <>
+      <div class='grid grid-cols-3 gap-4 divide-x'>
+        <div class='col-span-1'>
+          <div>TOP COMMENTS</div>
+        </div>
+        <div class='col-span-1 px-4 p-6'>
+          <div class='h-screen flex items-center justify-around '>
+            <h1 class='mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white'>
+              Select Ticker
+            </h1>
+            <div class='form-control w-full max-w-xs'>
+              <input
+                onChange={onChange}
+                type='text'
+                placeholder='ex: TSLA'
+                class='input input-bordered w-full max-w-xs'
+              />
+              <button
+                onClick={onSubmit}
+                type='submit'
+                class='bg-indigo-500 flex-none rounded-md px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500 !important'
+              >
+                Search
+              </button>
+            </div>
+          </div>
+        </div>
+        <div class='col-span-1'>
+          <div class='chat'>
+            <Chat />
           </div>
         </div>
       </div>
-      <div class='col-span-1'>
-        <div class='chat'>
-          <Chat />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
